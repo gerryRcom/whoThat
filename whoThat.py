413f5ef6ae942d37208519f9211c3c35c13c2be7
gerryR <gerryr@gerryr.com>
2023-07-07 22:37:09 +0100

    Code to query geolite for country of IP added

--- whoThat.py
+++ whoThat.py
@@ -2,20 +2,32 @@
 # 
 import os
 import re
+import json
+import requests
+from requests.auth import HTTPBasicAuth
 
 # Pull creds for geoip service from config file.
-def getCreds():
-    with open('whoThat.config') as credFile:
-        for line in credFile:
+def getConfig():
+    configContent = []
+    with open('whoThat.config') as configFile:
+        for line in configFile:
+            configContent.append(line)
+    return(configContent)
 
-            print(line.strip())
+# query geolite for the IP's country and just return the country name in English
+def queryLocation():
+    geoCreds = getConfig()
+    response = requests.get('https://geolite.info/geoip/v2.1/country/109.76.171.49?pretty',auth=HTTPBasicAuth(geoCreds[0].strip(), geoCreds[1].strip()))
+    responseJson = json.loads(response.text)
+    # just return the country value from the JSON content
+    return(responseJson["country"]["names"]["en"])
 
 # extract IPs from log file and tally amount of time's they feature.
 with open('./logs/access.log.1') as file:
     uniqueIP = {}
     for line in file:
         ip = re.findall(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', line)
-        print(ip[0])
+        # increment by one if ip is already in the dict, if it's not, add it to the dict.
         if ip[0] in uniqueIP:
             uniqueIP[ip[0]] += 1
         else:
@@ -23,12 +35,7 @@ with open('./logs/access.log.1') as file:
 
 
     print(uniqueIP)
-getCreds()
+
+print(queryLocation())
 
 
-
-    
-
-
-
-# 
